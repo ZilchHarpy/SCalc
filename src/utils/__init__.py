0b4721055ad75ebe9402aac2e92ffc3f0a415722
ZilchHarpy <102772898+ZilchHarpy@users.noreply.github.com>
2026-02-14 00:19:09 -0300

    src/core/statistics.py atualizada com a inclusão de src/utils/parses.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -3,3 +3,9 @@ Módulo de Utilidades
 
 Contém funções auxiliares e configurações gerais.
 """
+
+from .parses import eh_erro_instrumental
+
+__all__ = [
+    'eh_erro_instrumental'
+]
---
+++ src/utils/parses.py
@@ -0,0 +1,55 @@
+"""
+Módulo de parsing e extração de informações de nomes de colunas em DataFrames.
+"""
+
+import re
+from typing import Optional
+
+def extrair_prefixo(nome_coluna: str) -> Optional[str]:
+    """
+    Extrai o prefixo alfabético de uma coluna (ex: 'x1' -> 'x'; 'temp2' -> 'temp)
+
+    Args:
+        nome_coluna (str): Nome da coluna a analisar
+    
+    Returns:
+        str: Prefixo da variável, ou None se não encontrado
+    
+    Examples:
+        >>> extrair_prefixo('x1')
+        'x'
+        >>> extrair_prefixo('i_err')
+        'i'
+        >>> extrair_prefixo('temperatura2')
+        'temperatura'
+        >>> extrair_prefixo('123')
+        None
+    """
+    if not isinstance(nome_coluna, str):
+        return None
+    
+    match = re.match(r'^([a-zA-Z]+)', nome_coluna.strip())
+    return match.group(1) if match else None
+
+def eh_erro_instrumental(nome_coluna: str) -> bool:
+    """
+    verifica se uma coluna representa um erro instrumental
+
+    Args:
+        nome_coluna (str): Nome da coluna a analisar
+
+    Returns:
+        bool: True se for coluna de erro instrumental, False caso contrário
+
+    Examples:
+        >>> eh_erro('i_err')
+        True
+        >>> eh_erro('I_Err')
+        True
+        >>> eh_erro('y1')
+        False
+    """
+    if not isinstance(nome_coluna, str):
+        return False
+    
+    return ('err' in nome_coluna.lower() or 'error' in nome_coluna.lower() or 'erro' in nome_coluna.lower()) and ('i' in nome_coluna.lower() or 'instr' in nome_coluna.lower()  or 'ins' in nome_coluna.lower() or 'instrumental' in nome_coluna.lower() or 'instrument' in nome_coluna.lower())
